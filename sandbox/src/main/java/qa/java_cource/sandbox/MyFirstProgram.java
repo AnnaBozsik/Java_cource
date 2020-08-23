61fbb0a6816d727c8cc8377f47fb8cc48cd8687c
Anna Bozsik <59634867+AnnaBozsik@users.noreply.github.com>
2020-08-22 17:06:16 -0700

    Создан пакет для моей первой программы

--- sandbox/src/main/java/qa/java_cource/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/qa/java_cource/sandbox/MyFirstProgram.java
@@ -1,4 +1,6 @@
-public class MyFirstProgram{
+package qa.java_cource.sandbox;
+
+public class MyFirstProgram {
     public static void main(String[] args) {
         System.out.println("Hello, world!");
     }
