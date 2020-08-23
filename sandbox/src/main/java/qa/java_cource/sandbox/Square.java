9f3108c4f77a4b007af1e361a7dc8df686e6f3aa
Anna Bozsik <59634867+AnnaBozsik@users.noreply.github.com>
2020-08-22 21:57:12 -0700

    Добавили функции и методы

---
+++ sandbox/src/main/java/qa/java_cource/sandbox/Square.java
@@ -0,0 +1,15 @@
+package qa.java_cource.sandbox;
+
+public class Square {
+
+  public double l;
+
+  public Square(double l) {
+    this.l = l;
+  }
+
+  public double area () {
+    return this.l * this.l;
+  }
+
+}
