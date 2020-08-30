c828ddd856e8365234049eefd5e909383195073e
Anna Bozsik <59634867+AnnaBozsik@users.noreply.github.com>
2020-08-29 21:10:20 -0700

    Добавили тест для удаления групп, рефактор

---
+++ addressbook-web-tests/src/test/java/qa/java_cource/addressbook/GroupDeletionTests.java
@@ -0,0 +1,15 @@
+package qa.java_cource.addressbook;
+
+import org.testng.annotations.Test;
+
+public class GroupDeletionTests extends TestBase {
+
+  @Test
+  public void testGroupDeletion() throws Exception {
+    gotoGroupPage();
+    selectGroup();
+    deleteSelectedGroups();
+    returnToGroupPage();
+  }
+
+}
