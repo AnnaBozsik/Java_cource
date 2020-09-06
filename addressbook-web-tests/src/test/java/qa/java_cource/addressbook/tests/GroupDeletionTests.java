882c1e4beed3fe0823636329722e47f4776cee10
Anna Bozsik <59634867+AnnaBozsik@users.noreply.github.com>
2020-09-06 16:40:42 -0700

    Реализована проверка и обеспечение выполнения предусловий

--- addressbook-web-tests/src/test/java/qa/java_cource/addressbook/tests/GroupDeletionTests.java
+++ addressbook-web-tests/src/test/java/qa/java_cource/addressbook/tests/GroupDeletionTests.java
@@ -1,15 +1,18 @@
 package qa.java_cource.addressbook.tests;
 
 import org.testng.annotations.Test;
+import qa.java_cource.addressbook.model.GroupData;
 
 public class GroupDeletionTests extends TestBase {
 
   @Test
   public void testGroupDeletion() throws Exception {
     app.getNavigationHelper().gotoGroupPage();
+    if (! app.getGroupHelper().isThereAGroup()) {
+      app.getGroupHelper().createGroup(new GroupData("test_1", null, null));
+    }
     app.getGroupHelper().selectGroup();
     app.getGroupHelper().deleteSelectedGroups();
     app.getGroupHelper().returnToGroupPage();
   }
-
 }
