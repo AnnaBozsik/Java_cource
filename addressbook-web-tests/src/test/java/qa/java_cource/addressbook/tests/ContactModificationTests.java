ee670bd000042c31237e8bd4832b8051b16f6b9e
Anna Bozsik <59634867+AnnaBozsik@users.noreply.github.com>
2020-09-17 23:21:40 -0700

    Д.з.№9 Добавила условие, что удаляется/модифицируется последняя строка в списке

--- addressbook-web-tests/src/test/java/qa/java_cource/addressbook/tests/ContactModificationTests.java
+++ addressbook-web-tests/src/test/java/qa/java_cource/addressbook/tests/ContactModificationTests.java
@@ -13,7 +13,7 @@ public class ContactModificationTests extends TestBase {
       app.getNavigationHelper().gotoHome();
     }
     int before = app.getContactHelper().getContactCount();
-    app.getContactHelper().selectContact();
+    app.getContactHelper().selectContact(before - 1);
     app.getContactHelper().initContactEditing();
     app.getContactHelper().fillContactForm(new ContactData("Anna", "Bozsik", "925-961-6101", "ann.bozsik@gmail.com", null)/*, false*/);
     app.getContactHelper().initContactUpdate();
