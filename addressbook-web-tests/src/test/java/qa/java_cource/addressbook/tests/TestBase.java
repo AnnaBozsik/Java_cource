14fab2666ed56e89acb20fa382ea709d0617dcf8
Anna Bozsik <59634867+AnnaBozsik@users.noreply.github.com>
2020-09-30 19:56:15 -0700

    Д.з 13

--- addressbook-web-tests/src/test/java/qa/java_cource/addressbook/tests/TestBase.java
+++ addressbook-web-tests/src/test/java/qa/java_cource/addressbook/tests/TestBase.java
@@ -1,22 +1,44 @@
 package qa.java_cource.addressbook.tests;
 
 import org.openqa.selenium.remote.BrowserType;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
 import qa.java_cource.addressbook.appmanager.ApplicationManager;
 
+import java.lang.reflect.Member;
+import java.lang.reflect.Method;
+import java.util.Arrays;
+
 public class TestBase {
 
-  protected static final ApplicationManager app = new ApplicationManager(BrowserType.FIREFOX);
+  Logger logger = LoggerFactory.getLogger(TestBase.class);
+
+  protected static ApplicationManager app;
+
 
   @BeforeSuite (alwaysRun = true)
   public void setUp() throws Exception {
+    app = new ApplicationManager(System.getProperty("browser", BrowserType.CHROME));
     app.init();
   }
 
   @AfterSuite (alwaysRun = true)
   public void tearDown() throws Exception {
     app.stop();
+  }
+
+  @BeforeMethod
+  public void logTestStart(Method m, Object[] p) {
+    logger.info("Start test " + m.getName() + "with parameters " + Arrays.asList(p));
+  }
+
+  @AfterMethod (alwaysRun = true)
+  public void logTestStop(Method m, Object[] p) {
+    logger.info("Stop test " + m.getName() + "with parameters " + Arrays.asList(p));
   }
 
 }
