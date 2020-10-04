767352a47bbb97ad5b5682bd90cb6c19248342b4
Anna Bozsik <59634867+AnnaBozsik@users.noreply.github.com>
2020-10-03 22:48:43 -0700

    Отключали проверки UI

--- addressbook-web-tests/src/test/java/qa/java_cource/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/qa/java_cource/addressbook/tests/GroupCreationTests.java
@@ -58,6 +58,7 @@ public class GroupCreationTests extends TestBase {
     Groups after = app.db().groups();
     assertThat(after, equalTo(
             before.withAdded(group.withId(after.stream().mapToInt((g) -> g.getId()).max().getAsInt()))));
+    verifyGroupListInUI();
   }
 
   @Test
@@ -69,5 +70,6 @@ public class GroupCreationTests extends TestBase {
     assertThat(app.group().count(), equalTo(before.size()));
      Groups after = app.db().groups();
     assertThat(after, equalTo(before));
+    verifyGroupListInUI();
   }
 }
