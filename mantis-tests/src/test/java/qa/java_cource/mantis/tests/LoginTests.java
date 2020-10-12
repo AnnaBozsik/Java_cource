c747c0c455b15818f153292a399c7b1ca24235f9
Anna Bozsik <59634867+AnnaBozsik@users.noreply.github.com>
2020-10-12 00:25:57 -0700

    Занятие 8.4

---
+++ mantis-tests/src/test/java/qa/java_cource/mantis/tests/LoginTests.java
@@ -0,0 +1,18 @@
+package qa.java_cource.mantis.tests;
+
+import org.testng.annotations.Test;
+import qa.java_cource.mantis.appmanager.HttpSession;
+
+import java.io.IOException;
+
+import static org.testng.Assert.assertTrue;
+
+public class LoginTests extends TestBase {
+
+  @Test
+  public void testLogin() throws IOException {
+    HttpSession session = app.newSession();
+    assertTrue(session.login("administrator", "root"));
+    assertTrue(session.isLoggedInAs("administrator"));
+  }
+}
