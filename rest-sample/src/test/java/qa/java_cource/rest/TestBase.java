99ff1ebac20426a23985a1e36860a760ed11c898
Anna Bozsik <59634867+AnnaBozsik@users.noreply.github.com>
2020-11-18 01:17:06 -0800

    Д.з 20

---
+++ rest-sample/src/test/java/qa/java_cource/rest/TestBase.java
@@ -0,0 +1,31 @@
+package qa.java_cource.rest;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonParser;
+import org.apache.http.client.fluent.Executor;
+import org.apache.http.client.fluent.Request;
+import org.testng.SkipException;
+
+import java.io.IOException;
+
+
+public class TestBase {
+
+  public Executor getExecutor() {
+    return Executor.newInstance().auth("288f44776e7bec4bf44fdfeb1e646490", "");
+  }
+
+  public boolean isIssueOpen(int issueId) throws IOException {
+    String json = getExecutor().execute(Request.Get("https://bugify.stqa.ru/api/issues/" + issueId + ".json"))
+            .returnContent().asString();
+    JsonElement parsed = new JsonParser().parse(json);
+    String status = parsed.getAsJsonObject().get("state_name").getAsString();
+    System.out.println(status);
+    return status.equals("Open");
+  }
+
+  public void skipIfNotFixed(int issueId) throws IOException {
+    if (isIssueOpen(issueId)) {
+      throw new SkipException("Ignored because of issue " + issueId);
+    }
+  }
+}
